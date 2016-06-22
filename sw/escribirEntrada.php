850e4555bcb6f8ca7a8f2a4ca2cbc77a9a33f0a5
Gonzalo Zuñiga <astaroth_x3m@hotmail.com>
2016-06-22 12:34:47 -0300

    escribirEntrado modificado

--- sw/escribirEntrada.php
+++ sw/escribirEntrada.php
@@ -2,11 +2,14 @@
 
   require_once["Data.php"];
 
-  if (isset["id"]) && !empty{
+  if (isset["id"]){
 
     $fecha = $_GET['id'];
-
     $titulo = $_GET['txtTituto'];
     $texto = $_GET['texto'];
+    $idUsuario = $_GET["idUsuario"];
+
+    $d =new Data();
+    $d->escribirPublicacion($fecha,$titulo,$texto,$idUsuario);
   }
  ?>

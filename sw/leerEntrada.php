29037c783cdd2ee18dea9b261e0077346b3bec4c
nicolasahu <niko_jara_ahumada@live.cl>
2016-06-23 17:02:09 -0300

    actualizado

--- sw/leerEntrada.php
+++ sw/leerEntrada.php
@@ -1,12 +1,37 @@
 <?php
 require_once"Data.php";
 
-if(isset($_GET["btnLeerEntrada"])){
-  $id=$_GET["txtId"];
+if(isset($_GET["titulo"])){
+  $titulo=$_GET["titulo"];
   $d =new Data();
-  $d->getEntrada($id);
-}else {
-  echo "error al registrar";
-  header("location: ../index.php");
+  $existe=$d->getEntrada($titulo);
+  if ($existe==0) {
 
+    echo "<entradas>";
+    echo "<fecha>";
+    echo "</fecha>";
+    echo "<titulo>";
+    echo "</titulo>";
+    echo "<texto>";
+    echo "Esta publicacion no existe";
+    echo "</texto>";
+    echo "<usuario>";
+    echo "</usuario>";
+    echo "</entradas>";
+  }
+}else {
+
+  echo "<entradas>";
+  echo "<fecha>";
+  echo "</fecha>";
+  echo "<titulo>";
+  echo "</titulo>";
+  echo "<texto>";
+  echo "error al retornar dato";
+  echo "</texto>";
+  echo "<usuario>";
+  echo "</usuario>";
+  echo "</entradas>";
+
+}
  ?>
